875ce6dcff89f63fd2e37bf2971d6b0a7312d55d
jooliewallace <74530322+jooliewallace@users.noreply.github.com>
2024-04-16 09:56:31 +0100

    change drone input controls

---
+++ Assets/PlatformAlarm.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class RandomSoundPlayer : MonoBehaviour
+{
+    public AudioClip soundClip; 
+    public bool playSound = true; 
+    public float minDelay = 120.0f; 
+    public float maxDelay = 240.0f; 
+    public float volume = 1.0f; 
+    private bool hasPlayed = false;
+
+    void Start()
+    {
+        if (playSound)
+        {
+            // Generate a random delay between the min and max durations
+            float randomDelay = UnityEngine.Random.Range(minDelay, maxDelay); 
+            Invoke("PlaySound", randomDelay);
+        }
+    }
+
+    void PlaySound()
+    {
+        if (!hasPlayed && playSound)
+        {
+            GameObject soundObject = new GameObject("TempSoundObject");
+            AudioSource audioSource = soundObject.AddComponent<AudioSource>();
+
+            audioSource.volume = volume;
+
+            audioSource.PlayOneShot(soundClip);
+
+            // Destroy the temporary GameObject after the sound has finished playing
+            Destroy(soundObject, soundClip.length);
+
+            hasPlayed = true;
+        }
+    }
+}
